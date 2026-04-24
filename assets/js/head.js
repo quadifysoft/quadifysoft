f637778b9ea32e06d75052233c05a55c6ff4fc2f
Milan <tvoj@email.com>
2026-04-24 08:08:34 +0200

    * Sredjujem analitiku

--- assets/js/head.js
+++ assets/js/head.js
@@ -1,4 +1,4 @@
-﻿window.dataLayer = window.dataLayer || [];
+window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 
 if (window.location.hostname.endsWith('github.io')) {
